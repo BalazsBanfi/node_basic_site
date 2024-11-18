3c4b0a0ece4a09b09da9c6bad3595b25e50b7640
BalazsBanfi <banfibalazs81@gmail.com>
2024-11-18 13:24:38 +0100

    add 404 page

--- index.js
+++ index.js
@@ -4,7 +4,33 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
-    res.end('Hello World!');
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data);
+        })
+    } else if (req.url === '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data);
+        })
+    } else if (req.url === '/contact-me') {
+        fs.readFile(path.join(__dirname, 'public', 'contact-me.html'), (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data);
+        })
+    } else {
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(data);
+        })
+    }
+
+
 });
 
 const PORT = process.env.PORT || 5000;
